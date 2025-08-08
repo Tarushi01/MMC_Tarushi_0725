b877530bbb03d737a132bd231b2bf43013f00057
Tarushi01 <136785681+Tarushi01@users.noreply.github.com>
2025-08-08 07:50:58 +0000

    Add files via upload

---
+++ routerdemo1/src/app/components/contactus/locations/east/east.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-east',
+  imports: [],
+  templateUrl: './east.html',
+  styleUrl: './east.css'
+})
+export class East {
+
+}
